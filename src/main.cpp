91f0793c79c478e2a6c7c98d53c7f1f63802a22d
Guilherme Torres <guilhermetorres97@gmail.com>
2021-03-03 13:55:55 -0300

    project started

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <particle_source.hpp>
+#include <shader.hpp>
+
+int main() {
+    printf("Welcome to particles!\n");
+    return 0;
+}
---
+++ src/particle_source.cpp
@@ -0,0 +1,1 @@
+#include <particle_source.hpp>
