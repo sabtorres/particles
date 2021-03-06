5934855f0f4211b9b81584b849b343fcc95ce97c
Guilherme Torres <guilhermetorres97@gmail.com>
2021-03-06 18:19:22 -0300

    turned off depth testing, things look good

--- src/main.cpp
+++ src/main.cpp
@@ -39,9 +39,6 @@ int main() {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     glClearColor(0.1, 0.1, 0.1, 1.0);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glBlendEquation(GL_FUNC_ADD);
